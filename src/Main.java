34adb29207da5339aece212e231247bfd4c9f988
Carina <cncnewton37@gmail.com>
2018-09-18 17:02:52 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,47 @@
+public class Main {
+
+    public static void main(String[] args) {
+// This is my first Java application with Fi
+//        This is my second comment in this application
+// /* Multi line comment*/
+// I can put my comment on any line
+
+        }
+/*
+for(int count=1; count<11; count++}
+    if (count==5){
+    System.out.println("Woooooooooohoooo!!");
+}
+else {
+        System.out.println(count);
+    }
+}
+
+
+
+
+int count = 1;
+        do{
+        if(count==5)(
+        System.out.println("Woooooohoooo"!!);
+        }
+        else{
+    System.out.println(count);
+    } count++;
+
+                } while (count<=10);*/
+
+
+
+
+
+while(count<=10)
+int count=1; 
+    {
+        If(count==5)
+                System.out.println("Woohoo"!!);}
+                else{
+        Systems.out.println(count);
+        count++;
+    }
+}
